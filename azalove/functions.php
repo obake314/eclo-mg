7c61e22dcff76cd44acf4e1f298582c2cb29a9c2
Okazaki <149638097+havaskanda@users.noreply.github.com>
2026-06-09 11:06:33 +0900

    削除

--- azalove/functions.php
+++ azalove/functions.php
@@ -114,18 +114,100 @@ function azarashilove_async_yakuhanjp( $html, $handle ) {
     return $html;
 }
 
-/**
- * Load theme files
- */
-require get_template_directory() . '/inc/custom-header.php';
-require get_template_directory() . '/inc/template-tags.php';
-require get_template_directory() . '/inc/template-functions.php';
-require get_template_directory() . '/inc/block-patterns.php';
-require get_template_directory() . '/inc/customizer.php';
+// --- template tags ---
 
-if ( defined( 'JETPACK__VERSION' ) ) {
-	require get_template_directory() . '/inc/jetpack.php';
+if ( ! function_exists( 'azarashilove_posted_on' ) ) :
+	function azarashilove_posted_on() {
+		$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
+		if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
+			$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
+		}
+		$time_string = sprintf(
+			$time_string,
+			esc_attr( get_the_date( DATE_W3C ) ),
+			esc_html( get_the_date() ),
+			esc_attr( get_the_modified_date( DATE_W3C ) ),
+			esc_html( get_the_modified_date() )
+		);
+		echo '<div class="posted-on">' . sprintf( esc_html_x( '%s', 'post date', 'azarashilove' ), $time_string ) . '</div>';
+	}
+endif;
+
+if ( ! function_exists( 'azarashilove_posted_by' ) ) :
+	function azarashilove_posted_by() {
+		$byline = sprintf(
+			esc_html_x( 'by %s', 'post author', 'azarashilove' ),
+			'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
+		);
+		echo '<span class="byline"> ' . $byline . '</span>'; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+	}
+endif;
+
+if ( ! function_exists( 'azarashilove_entry_footer' ) ) :
+	function azarashilove_entry_footer() {
+		if ( 'post' === get_post_type() ) {
+			$categories_list = get_the_category_list( esc_html__( '', 'azarashilove' ) );
+			if ( $categories_list ) {
+				printf( '<div class="cat-links">' . esc_html__( '%1$s', 'azarashilove' ) . '</div>', $categories_list );
+			}
+			$tags_list = get_the_tag_list( '', esc_html_x( '', 'list item separator', 'azarashilove' ) );
+			if ( $tags_list ) {
+				printf( '<span class="tags-links">' . esc_html__( '%1$s', 'azarashilove' ) . '</span>', $tags_list );
+			}
+		}
+		if ( ! is_single() && ! post_password_required() && ( comments_open() || get_comments_number() ) ) {
+			echo '<span class="comments-link">';
+			comments_popup_link(
+				sprintf(
+					wp_kses( __( 'Leave a Comment<span class="screen-reader-text"> on %s</span>', 'azarashilove' ), array( 'span' => array( 'class' => array() ) ) ),
+					wp_kses_post( get_the_title() )
+				)
+			);
+			echo '</span>';
+		}
+	}
+endif;
+
+if ( ! function_exists( 'azarashilove_post_thumbnail' ) ) :
+	function azarashilove_post_thumbnail() {
+		if ( post_password_required() || is_attachment() || ! has_post_thumbnail() ) {
+			return;
+		}
+		if ( is_singular() ) : ?>
+			<div class="post-thumbnail"><?php the_post_thumbnail(); ?></div>
+		<?php else : ?>
+			<a class="post-thumbnail" href="<?php the_permalink(); ?>" aria-hidden="true" tabindex="-1">
+				<?php the_post_thumbnail( 'post-thumbnail', array( 'alt' => the_title_attribute( array( 'echo' => false ) ) ) ); ?>
+			</a>
+		<?php endif;
+	}
+endif;
+
+if ( ! function_exists( 'wp_body_open' ) ) :
+	function wp_body_open() {
+		do_action( 'wp_body_open' );
+	}
+endif;
+
+// --- template functions ---
+
+function azarashilove_body_classes( $classes ) {
+	if ( ! is_singular() ) {
+		$classes[] = 'hfeed';
+	}
+	if ( ! is_active_sidebar( 'sidebar-1' ) ) {
+		$classes[] = 'no-sidebar';
+	}
+	return $classes;
 }
+add_filter( 'body_class', 'azarashilove_body_classes' );
+
+function azarashilove_pingback_header() {
+	if ( is_singular() && pings_open() ) {
+		printf( '<link rel="pingback" href="%s">', esc_url( get_bloginfo( 'pingback_url' ) ) );
+	}
+}
+add_action( 'wp_head', 'azarashilove_pingback_header' );
 
 /**
  * =============================================================================
